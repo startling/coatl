b3d494aac9a71f0fe26cfa09d2e34e0c6f2b5d56
startling <tdixon51793@gmail.com>
2013-08-11 21:09:03 -0400

    First stab at an interactive thing.

---
+++ Language/Coatl/Interactive.hs
@@ -0,0 +1,87 @@
+{-# Language FlexibleContexts #-}
+module Language.Coatl.Interactive where
+-- base
+import Control.Applicative
+import Data.Monoid
+-- transformers
+import Control.Monad.Identity
+import Control.Monad.IO.Class
+import Control.Monad.Error
+import Control.Monad.Trans
+-- mtl
+import Control.Monad.State.Strict
+import Control.Monad.Reader
+-- trifecta
+import Text.Trifecta
+-- lens
+import Control.Lens
+-- haskeline
+import System.Console.Haskeline
+-- either
+import Control.Monad.Trans.Either
+-- monad-loops
+import Control.Monad.Loops
+-- coatl
+import Language.Coatl.Syntax
+import Language.Coatl.Abstract
+import Language.Coatl.Parse
+import Language.Coatl.Check
+import Language.Coatl.Check.Types
+import Language.Coatl.Evaluate
+
+data Command
+  = TypeC (Syntax Span Identifier)
+  | EvalC (Syntax Span Identifier)
+  | QuitC
+  deriving
+  ( Eq
+  , Show
+  )
+
+command :: DeltaParsing f => f (Maybe Command)
+command = Just <$> cs <|> Nothing <$ spaces where
+  cs = 
+          TypeC <$> (symbol ":t" *> expression)
+      <|> EvalC <$> expression
+      <|> QuitC <$  symbol ":q"
+
+interactive :: MonadException m => m (Environment Span Canonical)
+interactive = flip execStateT standard . runInputT settings
+  . (() <$) . iterateWhile id $ do
+    line <- getInputLine "> "
+    case parseString command mempty <$> line of
+      Nothing -> return True
+      Just (Failure f) -> True <$ liftIO (print f)
+      Just (Success Nothing) -> return True
+      Just (Success (Just QuitC)) -> return False
+      Just (Success (Just (TypeC s))) -> True <$ lift (showType s)
+      Just (Success (Just (EvalC s))) -> True <$ lift (showEval s)
+  where
+    settings = defaultSettings
+
+showType ::
+  ( MonadIO m
+  , MonadState (Environment a Canonical) m )
+  => Syntax a Identifier -> m ()
+showType s = either (liftIO . print) return <=< runEitherT $
+  represent (fmap canonicalize s)
+    >>= \r -> case preview _CInfer r of
+      Nothing -> throwError ["Uninferrable type."]
+      Just ty -> lift get >>= runReaderT (infer ty) . Checking id
+        >>= liftIO . putStrLn . pretty
+
+showEval ::
+  ( MonadIO m
+  , MonadState (Environment a Canonical) m )
+  => Syntax a Identifier -> m ()
+showEval s = either (liftIO . print) return <=< runEitherT $
+  represent (fmap canonicalize s)
+    >>= \r -> case preview _CInfer r of
+      Nothing -> throwError ["Uninferrable type."]
+      Just ty -> lift get >>= runReaderT (infer ty) . Checking id
+        >> lift get >>= runReaderT (evaluate r) . view definitions
+          >>= liftIO . putStrLn . pretty
+
+-- TODO: make this prettier
+pretty :: Value Canonical -> String
+pretty = show
