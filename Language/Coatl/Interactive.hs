843e9793da9b520ac8ab8f2ada8a829d3343a8e3
startling <tdixon51793@gmail.com>
2013-08-13 18:00:35 -0400

    Added some documentation to the interactive shell.

--- Language/Coatl/Interactive.hs
+++ Language/Coatl/Interactive.hs
@@ -1,4 +1,5 @@
 {-# Language FlexibleContexts #-}
+-- | An interactive shell for checking and interpreting coatl.
 module Language.Coatl.Interactive where
 -- base
 import Control.Applicative
@@ -30,16 +31,22 @@ import Language.Coatl.Check
 import Language.Coatl.Check.Types
 import Language.Coatl.Evaluate
 
+-- | A type for instructions to the interactive shell.
 data Command
   = TypeC (Term Span Identifier)
+    -- ^ Infer the type of some term.
   | EvalC (Term Span Identifier)
+    -- ^ Pretty-print the fully-evaluated form of a term.
   | LoadC FilePath
+    -- ^ Load something new into the environment.
   | QuitC
+    -- ^ Exit from the interactive shell.
   deriving
   ( Eq
   , Show
   )
 
+-- | Parse a command.
 command :: DeltaParsing f => f (Maybe Command)
 command = Just <$> cs <|> Nothing <$ spaces where
   cs = 
@@ -48,24 +55,28 @@ command = Just <$> cs <|> Nothing <$ spaces where
       <|> LoadC <$> (symbol ":l" *> some anyChar)
       <|> QuitC <$  symbol ":q"
 
-interactive :: MonadException m => m (Environment Span Canonical)
-interactive = flip execStateT standard . runInputT settings
-  . (() <$) . iterateWhile id $ do
-    line <- getInputLine . show . dullcyan . text $ "> "
-    case parseString (command <* eof) mempty <$> line of
-      Nothing -> return True
-      Just (Failure f) -> True <$ liftIO (print $ indent 2 f)
-      Just (Success Nothing) -> return True
-      Just (Success (Just QuitC)) -> return False
-      Just (Success (Just (TypeC s))) -> True <$ lift (showType s)
-      Just (Success (Just (EvalC s))) -> True <$ lift (showEval s)
-      Just (Success (Just (LoadC f))) -> True <$ lift (loadFile f)
+-- | Run the interactive shell, with some 'Environment' as state.
+interactive ::
+  ( MonadState (Environment Span Canonical) m
+  , MonadException m )
+  => m ()
+interactive = runInputT settings . (() <$) . iterateWhile id $ do
+  line <- getInputLine . show . dullcyan . text $ "> "
+  case parseString (command <* eof) mempty <$> line of
+    Nothing -> return True
+    Just (Failure f) -> True <$ liftIO (print $ indent 2 f)
+    Just (Success Nothing) -> return True
+    Just (Success (Just QuitC)) -> return False
+    Just (Success (Just (TypeC s))) -> True <$ lift (showType s)
+    Just (Success (Just (EvalC s))) -> True <$ lift (showEval s)
+    Just (Success (Just (LoadC f))) -> True <$ lift (loadFile f)
   where
     settings = defaultSettings
 
 handling :: MonadIO m => EitherT Doc m () -> m ()
 handling = either (liftIO . print . indent 2) return <=< runEitherT
 
+-- | Load a file into the interactive environment.
 loadFile ::
   ( MonadIO m
   , MonadState (Environment Span Canonical) m )
@@ -75,6 +86,7 @@ loadFile f = handling $ parseFromFileEx (many declaration) f
     Failure d -> throwError . indent 2 $ d
     Success s -> declarations . map (fmap canonicalize) $ s
 
+-- | Evaluate and show the type of a term.
 showType ::
   ( MonadIO m
   , MonadState (Environment a Canonical) m )
@@ -86,6 +98,7 @@ showType s = handling $
       Just ty -> lift get >>= runReaderT (infer ty) . Checking id
         >>= liftIO . print . indent 2 . pretty
 
+-- | Evaluate and pretty-print a term.
 showEval ::
   ( MonadIO m
   , MonadState (Environment a Canonical) m )
